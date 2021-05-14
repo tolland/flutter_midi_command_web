4727a4ad1dec176d279c9c7479433e454418b100
Morten Mortensen <morten@invisiblewrench.com>
2021-05-14 15:48:30 +0200

    example clean

--- lib/flutter_midi_command_web.dart
+++ lib/flutter_midi_command_web.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
-// import 'dart:js';
 
-@JS()
+@JS('midi_command')
 import 'package:js/js.dart';
 import 'dart:html' as html;
 import 'dart:typed_data';
@@ -10,72 +9,37 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:flutter_midi_command_platform_interface/flutter_midi_command_platform_interface.dart';
 
 class FlutterMidiCommandWeb extends MidiCommandPlatform {
-  html.Window _window;
+  late html.Window _window;
 
-  html.MidiAccess _access;
+  html.MidiAccess? _access;
 
   StreamController<Uint8List> _rxStreamController = StreamController<Uint8List>.broadcast();
   Stream<Uint8List> _rxStream = StreamController<Uint8List>.broadcast().stream;
   StreamController<String> _setupStreamController = StreamController<String>.broadcast();
-  Stream<String> _setupStream;
+  Stream<String>? _setupStream;
 
   /// A constructor that allows tests to override the window object used by the plugin.
-  FlutterMidiCommandWeb({@visibleForTesting html.Window window}) {
+  FlutterMidiCommandWeb({@visibleForTesting html.Window? window}) {
     _window = window ?? html.window;
     _requestMidiAccess();
   }
 
   /// Registers this class as the default instance of [UrlLauncherPlatform].
   static void registerWith(Registrar registrar) {
+    print("register web impl");
     MidiCommandPlatform.instance = FlutterMidiCommandWeb();
   }
 
   _requestMidiAccess() async {
+    print("request midi access");
     _window.navigator.requestMidiAccess({'sysex': true}).then((value) {
       print("succes $value");
       _access = value;
-      _access.on['statechange'].listen(_handleMIDIStateChange);
+      print('inputs ${_access!.inputs}');
+      print('outputs ${_access!.outputs}');
 
-
+      _access!.on['statechange'].listen(_handleMIDIStateChange);
     });
-
-    // JsFunction func = context['navigator']['requestMIDIAccess'];
-    // JsObject promise = func.apply([], thisArg: context['navigator']);
-    //
-    // promise.callMethod('then', [handler]);
-
-
-
-    final JS. w = new JsObject.fromBrowserObject(_window);
-    final JsObject n = w['navigator'];
-
-    if (n.hasProperty('requestMIDIAccess')) {
-      n.callMethod('requestMIDIAccess').callMethod('then', [
-        (JsObject midiAccess) {
-          print(midiAccess);
-          print(midiAccess.hasProperty('inputs'));
-          JsObject inputs = JsObject.fromBrowserObject(midiAccess['inputs']);
-          print(inputs);
-
-          // JsObject.fromBrowserObject(object)
-          // js.scoped(() {
-          //   String json = js.context.JSON.stringify(js.context.myMap);
-          //   Map map = JSON.parse(json);
-          //   // iterate on dart Map
-          // });
-
-          var inputValues = inputs.callMethod('values');
-          print(inputValues);
-          // JsObject inputIterator = inputValues.callMethod('foreach');
-          // print(inputIterator);
-          for (JsObject input in inputValues) {
-            print(input['name']);
-          }
-          // ;
-          // sendMiddleC(midiAccess, 1);
-        }
-      ]);
-    }
   }
 
   void handler(html.MidiAccess access) {
@@ -83,25 +47,26 @@ class FlutterMidiCommandWeb extends MidiCommandPlatform {
   }
 
   @override
-  Future<List<MidiDevice>> get devices async {
-    // JsObject i = new JsObject.fromBrowserObject(object)
-    // _access.inputs.forEach((key, value) {
-    //   print("input key ${key} ${value}");
-    //   // print( "Input port [type:'${element.type}'] id:'${element.id}' manufacturer:'${element.manufacturer}' name:'${element.name}' version:'${element.version}'");
-    //   // element.value.onMidiMessage.listen(_handleMidiIn);
-    //   // html.MidiPort inPort = element as html.MidiPort;
-    //   // value.on['midimessage'].listen(_handleMidiIn);
-    // });
-
-    // _access.outputs.forEach((key, value) {
-    //   print("output ${key} ${value}");
-    // });
-
+  Future<List<MidiDevice>?> get devices async {
     // var devs = await _methodChannel.invokeMethod('getDevices');
     // return devs.map<MidiDevice>((m) {
     //   var map = m.cast<String, Object>();
     //   return MidiDevice(map["id"], map["name"], map["type"], map["connected"] == "true");
     // }).toList();
+
+    _access!.inputs!.forEach((key, value) {
+      print("input key ${key} ${value}");
+      // print("Input port [type:'${element.type}'] id:'${element.id}' manufacturer:'${element.manufacturer}' name:'${element.name}' version:'${element.version}'");
+      // element.value.onMidiMessage.listen(_handleMidiIn);
+      // html.MidiPort inPort = element as html.MidiPort;
+      // value.on['midimessage'].listen(_handleMidiIn);
+    });
+
+    _access!.outputs!.forEach((key, value) {
+      print("output ${key} ${value}");
+    });
+
+    return Future.value(null);
   }
 
   _handleMIDIStateChange(evt) {
@@ -113,6 +78,14 @@ class FlutterMidiCommandWeb extends MidiCommandPlatform {
   _handleMidiIn(html.MidiMessageEvent evt) {
     print("midi in $evt");
   }
+
+  /// Starts scanning for BLE MIDI devices.
+  ///
+  /// Found devices will be included in the list returned by [devices].
+  Future<void> startScanningForBluetoothDevices() async {}
+
+  /// Stop scanning for BLE MIDI devices.
+  void stopScanningForBluetoothDevices() {}
 
   /// Connects to the device.
   @override
@@ -149,7 +122,7 @@ class FlutterMidiCommandWeb extends MidiCommandPlatform {
     // _rxStream ??= _rxChannel.receiveBroadcastStream().map<Uint8List>((d) {
     //   return Uint8List.fromList(List<int>.from(d));
     // });
-    _rxStream ??= _rxStreamController.stream;
+    _rxStream = _rxStreamController.stream;
     return _rxStream;
   }
 
@@ -157,8 +130,8 @@ class FlutterMidiCommandWeb extends MidiCommandPlatform {
   ///
   /// For example, when a new BLE devices is discovered.
   @override
-  Stream<String> get onMidiSetupChanged {
-    _setupStream ??= _setupStreamController.stream;
+  Stream<String>? get onMidiSetupChanged {
+    _setupStream = _setupStreamController.stream;
     return _setupStream;
   }
 }
